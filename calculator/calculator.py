ebce36d842365f2a926de9276287cac83e4f7bb0
vipinreyaroth <38558860+vipinreyaroth@users.noreply.github.com>
2018-11-29 14:20:11 +1100

    Updated the code after running a linter

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,9 +1,9 @@
 # A Calculator library containing basic math operations
 
 
-def add(first_itenm, second_item):
+def add(first_item, second_item):
     return first_item + second_item
 
 
-def substract(first_item, second_item):
+def subtract(first_item, second_item):
     return first_item - second_item
