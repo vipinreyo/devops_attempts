fd00909064b181667db71e8cebc78de91ff13501
vipinreyaroth <38558860+vipinreyaroth@users.noreply.github.com>
2018-11-29 14:16:25 +1100

    Added functions for addition and subtraction

---
+++ calculator/calculator.py
@@ -0,0 +1,9 @@
+# A Calculator library containing basic math operations
+
+
+def add(first_itenm, second_item):
+    return first_item + second_item
+
+
+def substract(first_item, second_item):
+    return first_item - second_item
